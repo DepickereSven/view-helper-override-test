10c67ef6d8962cc433c7d8697d57b341c678a0b9
sof_sepi <sven.depickere@sofico.be>
2020-06-11 13:33:23 +0200

    rename

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent } from './view/main.component';
 import { HelloComponent } from './view/hello.component';
-import { IMainViewHelperService, MAIN_VIEW_HELPER_SERVICE, MainViewHelpersConfig } from './view-helpers/IMainViewHelperService';
+import { IMainViewHelperService, MAIN_VIEW_HELPER_SERVICE, MainViewHelpersConfig } from './view-helpers/I-Main-View-Helper.service';
 
 @NgModule({
   imports: [BrowserModule],
--- src/app/main/view/main.component.ts
+++ src/app/main/view/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { IMainViewHelperService, MAIN_VIEW_HELPER_SERVICE } from '../view-helpers/IMainViewHelperService';
+import { IMainViewHelperService, MAIN_VIEW_HELPER_SERVICE } from '../view-helpers/I-Main-View-Helper.service';
 
 @Component({
   selector: 'main',
