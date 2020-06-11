1f0227559346344f821d9cd36f4ce1e2e91f71ff
sof_sepi <sven.depickere@sofico.be>
2020-06-11 13:24:52 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MainModule } from './main.module';
+import { MainModule } from './main/main.module';
 import { OverridableViewHelper } from './overridable-view.helper';
 
 @NgModule({
